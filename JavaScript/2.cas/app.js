ee7b72627406da1b2bdc1c6fd0e0dbd4a205505e
UBaksa <ujkanovicbakir03@gmail.com>
2022-10-15 19:26:11 +0200

    Tipvo podataka

---
+++ JavaScript/2.cas/app.js
@@ -0,0 +1,51 @@
+// Javascript moze prikazati podatke na nekoliko nacina:
+
+// 1.innerHTML
+
+document.getElementById("p").innerHTML = "Paragraf"
+
+// 2.write()
+document.write()//najcesci nacin za testiranje nekog koda
+
+// 3.alert()
+alert("Zdravo")
+
+// 4.console.log()-ispisivanje u konzoli 
+
+console.log("Poruka za ispis u konzoli")
+
+// Pravila nazivanja promenljivih:
+
+// Promenljiva u JavaScriptu mora da pocne sa nekom od sledecih znakova.
+
+// 1.Slovo(veliko ili malo ) A-Z ili a-z
+// 2.$ znak moze biti pocetni karakter neke promenljive
+// 3. _
+
+// Broj moze biti sadrzan u nekoj promenljivoj,ali ne sme promenljiva poceti brojem.
+
+// prom1 = "neki string" jeste korektno 
+// 2prom = "neki string" nije korektno
+
+// prosek ocena = 4.14 nije korektno
+prosekOcena = 4.14. // preporucljiv nacin zapisivanja promenljivih 
+prosek_ocena = 4.14 // takodje je ovo ispravno
+ProsekOcena = 4.14 // i ovo je ispravno
+
+
+// Postoje 4 nacina za deklarisanje neke promenljive:
+// 1. prom = 12 - dodeljivanje vrednsti bez da smo prethodno izvrsili deklaraciju 
+
+// 2. koriscenjem var keyword(rezervisane reci) - dodatak
+// var je koriscena na starijim browserima  (sve se manje koristi)
+
+// var a = 5
+
+// 3. Koriscenjem let keyword(rezervisane reci)
+// let se najcesce koristi kada postoji sansa da se vrednost date promenljive menja.
+
+// 4.Koriscenjem const keyword (rezervisane reci )- 
+// const se najcesce koristi definisanje konstanti koje nece menjati vrednost.
+
+// Promenljive odnosno varijable predstavljaju kontejnere za skladistenje vrednosti
+// dfdf
--- JavaScript/app.js
+++ JavaScript/app.js
@@ -55,5 +55,77 @@ let prom1; //deklarisanje promenljive
 prom1 = 54; //inicijalizacija (dodela vrednosti) promenljive
 
 // 6. null - je tip podatka nepostojecu promenljivu ili za promenljivu kojoj smo dodelili nekorektnu vrednost.
-let skola;
+skola = null
+console.log(skola)
 console.log(typeof skola);
+
+// 7.Symbol - novi tip podatka u javascriptu koji se koristi za dodeljivanje jedinstvene ili anonimne vrednosti.
+x = Symbol("Danas je lepo vreme.")
+console.log(x)
+y = Symbol("Danas je lepo vreme.")
+console.log(y)
+// Svaki je simbol razlicit iako ima isti tekst,razliciti su !
+console.log(x === y)
+
+// Primitivni tipovi podataka se koriste kada imamo promenljivu,varijablu,sa samo jednom vrednoscu .
+
+
+// Neprimitivni tipovi podataka //
+
+// Za promenljive (varijable) koje mogu skladistiti vise vrednosti,ili neke kompleksne vrednosti se koriste neprimitivni tipovi podataka. //
+
+// Kada ispitujemo tip podatka za neku neprimitivnu promenljivu u JavaScriptu dobijamo Object za tip.(type of)
+
+// 1.Nizovi (Arrays)-neprimitivnitip podatka koji nam sluzi skladistenje vise vrednosti.
+// U JavaScriptu niz moze sadrzati elemente razlicitog tipa.
+
+niz1 = ["Mitar", "Dzenan","Haris" ,"Hamed", "Nikola"]
+console.log(niz1)
+niz2 = ["Hamed",15,false]
+console.log(niz2)
+
+// Jos jedan nacin definisanja tipa:
+niz3 = new Array("Mitar", "Dzenan","Haris" ,"Hamed", "Nikola")
+console.log(niz3)
+
+// 2.Object- su glavni neprimitivni tipovi podataka u javascriptu.
+
+// Nacin da prikazemo odredjene vrednosti ako pricamo osobi(gde prikazujemo svojstva-vrednosti coveka/object-a)
+
+// Objekti se zapisuju ,po principu name:value pair (ime:vrednost par)
+
+// sintaakse za objekt su { } zagrade
+ object2 = new Object({
+  ime:"Amer",
+  prezime:"Honic",
+  godine:19,
+  skola:"DUNP",
+  punoletnost:true,
+ })
+console.log(object2.skola)
+
+obj1 = {
+  ime:"Mehmed",
+  prezime:"Kucevic",
+  godine:15,
+  skola:"Dva heroja",
+  punoletnost:false,
+}
+// index kod niza je od 0 do beskonacno,a ovamo je kako ga imenujemo
+
+console.log(obj1.skola);
+
+// 3.Setovi (Sets)-slicni nizovima sa razlikom da set ne moze da zadrzi duplikate.
+
+// setovi i mape se zapisuju putem NEW-a 
+set1 = new Set(["Alen","Hasan","Miona","Amir","Alen"]) //nisu dozvoljeni publikati(iskuliraju se jednostavno)
+console.log(set1)
+
+// 4.Mape-su slicni Objectima sa nesto drugacijim nacinom definisanja.
+
+map1 = new Map([
+  ["jabuke",50],
+  ["kruske",60],
+  ["jagode",80],
+])
+console.log(map1)
