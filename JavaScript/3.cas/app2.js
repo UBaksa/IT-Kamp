c21d78bed94f413aa035128fbc52a2161cb62950
UBaksa <ujkanovicbakir03@gmail.com>
2022-10-20 23:47:16 +0200

    3.cas-edit

---
+++ JavaScript/3.cas/app2.js
@@ -0,0 +1,47 @@
+// OPERATORI U JAVASCRIPTU //
+//Podelicemo operatore u 6 grupa.
+
+// 1.Aritmeticki operatori.(Aritmetic operators)
+// 2.Operatori dodele vrednosti(Assigments operators)
+// 3.String operatori(String operators)
+// 4.Operatori poredjenja (Comparison operators)
+// 5.Logicki operatori(Logical operators)
+// 6.Tip Operatori (Type operators.)
+
+// 1.Aritmeticki operatori.(Aritmetic operators)
+// 1.1 + operator sabiranja
+console.log(4 + 5);
+// 1.2 - -  operator oduzimanja
+console.log(4-5);
+// 1.3 - * operator mnozenja
+console.log(4*5)
+// 1.4 - / operator deljenja
+console.log(20/5)
+//1.5 - ** operator stepenovanja 
+console.log(2 ** 3)//2 je osnova a 3 je stepen
+// 1.6 - % operator za ostatak pri deljenju - Modulo/moduo
+console.log(11 % 3)//2
+console.log(12 % 3 )//0
+
+// Uslov za parnost:
+// i % 2 = 0
+// Uslov za neparnost
+// i % 2 = 1
+// 1.7 - ++ increment (povecava vrednost za 1)
+
+let t = 5
+t++ // x = x + 1
+// nakon 33 linije koda kad se bude koristilo t u console log odma ce bit 6 i unosimo samo t
+console.log(t)
+
+let o = 11
+// prefiksni operator
+console.log(++o)
+// ovo je odma da bude
+// 1.8 - -- decrement (smanjuje vrednost za 1)
+ let m = 99
+ m-- // z=z-1
+ console.log(m)
+//  prefiksni operator
+let w = 65
+console.log(--w)
