d40f86df2909479075c95f95db9c836eeb992e6e
UBaksa <ujkanovicbakir03@gmail.com>
2022-10-28 19:32:56 +0200

    domaci1.end

--- JavaScript/domaci/domaci1.js
+++ JavaScript/domaci/domaci1.js
@@ -8,19 +8,42 @@
 // 4.
 // Suma neparnih prirodnih brojeva od 10 do 20.
 // liveServer.settings.ignoreFiles":["**"]
-for(i=1;i<20;i++){
+// 1.
+for(let i=1;i<20;i++){
     if(i%2 !== 0){
     console.log(i)
     }
 }
-for(i=1;i<20;i=i+2){
+for(let i=1;i<20;i=i+2){
     console.log(i)
 }
-for(i=50;i<=100;i++){
+// 2/
+for(let i=50;i<=100;i++){
    if(i%5 ===0){
     console.log(i)
    }
    else{
     continue
    }
+}
+// 3.
+for(let i=1;i<=14;i++){
+    if(i*2>14){
+        continue
+    }
+    else if(i*2<6){
+        continue
+    }
+    else{
+        console.log(i)
+    }
+}
+// 4.
+for(let i=10;i<20;i+2){
+    if((i%2 !==0)*5){
+        console.log(i)
+    }
+    else{
+        continue
+    }
 }
