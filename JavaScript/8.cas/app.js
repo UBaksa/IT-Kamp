790977e17dcc2a74f27ac82aa29b43ebaacfa19e
UBaksa <ujkanovicbakir03@gmail.com>
2022-11-01 20:58:27 +0100

    8.cas java

---
+++ JavaScript/8.cas/app.js
@@ -0,0 +1,56 @@
+// 1.Prebrojati i sabrati brojeve deljive sa 5 u intervalu od 1 do n.
+// let n =+prompt("Unesite broj n");
+// let brojac = 0;
+// let suma = 0;
+// for(let broj = 1;broj <=n;broj++){
+//     if(broj%5 === 0){
+//         brojac++
+//         suma += broj;
+//     }
+
+// }
+// console.log("Ukupno ima " + brojac + "brojeva koji s deljivi sa 5.\n a njihova suma je: " + suma + ".")
+
+
+// 2.Neka se izvrsi iteracija od broja 99 do -99.
+// U svakoj iteraciji ispisati zbir brojeva deljivog sa 4 i broja 14.
+
+// for(let i=99;i>=-99;i--){
+//     if(i%4 ===0){
+//         console.log(i+14)
+//     }
+// }
+
+// let iteraator = 99
+// while(iteraotor > -100){
+// if(i%4 ===0){
+//     console.log(i+14)
+// }
+// iterator--;
+// }
+
+
+// 3.izracunati aritmeticku sredinu brojeva koji su deljivi sa 3.
+// iteracija se vrsi od 3 do 17.
+// let brojac = 0
+// let sredina = 0
+// for(let i=3;i<=17;i++){
+//     if(i%3 === 0){
+//         brojac++;
+//         sredina=sredina+i;
+//     }
+// }
+// console.log(sredina/brojac)
+// let number = 3;
+// let counter = 0;
+// let sum = 0;
+
+// while (number < 18){
+//     if(number%3===0){
+//         counter++
+//         sum += number
+//     }
+//     number++
+// }
+// console.log("Aritmeticka sredina brojeva deljivih brojem sa 3 iz intervala[3,17]" + sum/counter 
+)
