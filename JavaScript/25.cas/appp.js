97868660877ed80b1abca82da80dc47a5e3c881b
UBaksa <ujkanovicbakir03@gmail.com>
2022-12-20 20:34:43 +0100

    25.cas app2

---
+++ JavaScript/25.cas/appp.js
@@ -0,0 +1,37 @@
+// Number Properties!,konstante //
+
+
+// Sledecim konstantama mozemo pristupiti jedino pomocu Number:
+//Number.PropertyName
+
+// Napomena.
+// Konstante ne mogu biti primenjene na varijable.
+
+// 1.MAX_VALUE - Vraca najveci broj u JavaScriptu.
+
+const najveci = Number.MAX_VALUE
+
+console.log(najveci)
+
+// 2.MIN_VALUE - Vraca najmanji broj u JavaScriptu.
+
+
+const najmanji = Number.MIN_VALUE
+console.log(najmanji)
+
+
+// 3. POSITIVE_INFINITY -Vraca Infinity.
+
+const beskonacno = Number.POSITIVE_INFINITY
+console.log(beskonacno)
+
+// 4. NEGATIVE_INFINITY -Vraca -Infinity.
+
+const beskonacno1 = Number.NEGATIVE_INFINITY
+console.log(beskonacno1)
+
+
+// 5. NaN -Vraca NaN.
+
+const nijebroj = Number.NaN
+console.log(nijebroj)
