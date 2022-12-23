fcbd7fe61eac4396d610fc6c45363c6d2ba1c06c
UBaksa <ujkanovicbakir03@gmail.com>
2022-12-23 23:56:01 +0100

    domaci

---
+++ JavaScript/26.cas/domaci.js
@@ -0,0 +1,11 @@
+//  Domaci:
+// 1.
+// Napraviti funkciju koja izracunava obim kruga, gde se
+//  poluprecnik unosi od strane korisnika.
+
+// 2.
+// Napraviti funkciju koja izracunava povrsinu kruga, gde
+//  precnik predstavlja argument funkcije.
+
+// 3.
+// Napraviti funkciju za pretvaranje radijana u stepene.
