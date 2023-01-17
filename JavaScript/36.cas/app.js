71a11c17abbb6dde216ef3d4702d58301b319d6d
UBaksa <ujkanovicbakir03@gmail.com>
2023-01-17 20:57:46 +0100

    36.cas v2

--- JavaScript/36.cas/app.js
+++ JavaScript/36.cas/app.js
@@ -41,3 +41,76 @@ const novi2 = newArr.filter(function (element) {
   return typeof element === typeof true;
 });
 console.log(novi2);
+
+// Napraviti funkciju koja pravi niz koji ce iz postojeceg napraviti :
+// novi uz ispunjenje uslova:
+// value > 2 return vrednosti*7
+// value > 2 and 8 - return value*2;
+// Ostale vrednosti da ne uzima u obzir;
+// I na kraju treba novi niz vratiti sa godinama <10.
+
+const niz3 = niz.filter(function (element) {
+  return element > 2;
+});
+const niz4 = niz3.map(function (element) {
+  if (element < 8) {
+    return element * 2;
+  } else if (element > 2 && element > 8) {
+    return element * 7;
+  }
+});
+const novi5 = niz4.filter(function (element) {
+  return niz4;
+});
+
+// reduce() metoda vraca jednu vrednost.Iteracija se vrsi kroz ceo niz.
+
+// Sabrati sve elemente niza:
+
+// ova metoda uziam dva parametra,previousValue i currentValue.
+const zbirGodina = godine.reduce(
+  (previousValue, currentValue) => previousValue + currentValue
+);
+console.log(zbirGodina);
+
+// Dodati pocetnu vrednost 350;
+const iznos = godine.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  350
+);
+console.log(iznos);
+
+// na niz godine da se vrati prosecna starost godina.
+
+const prosek = godine.reduce(
+  (previousValue, currentValue) => previousValue + currentValue
+);
+console.log(prosek / godine.length);
+
+// Write a function that converts an array of values from miles to kilometres using the map method.
+// In the end,add the kilometres up in a new variable called "totalDistanceKilometers" and return this variable.
+
+const miles = [55, 78, 140, 121, 96, 100, 67];
+const kilometers = miles.map((previousValue) => previousValue * 1.6);
+const kilometerss = kilometers.reduce(function (previousValue, currentValue) {
+  return previousValue + currentValue;
+});
+console.log(kilometerss);
+
+const arra1 = [1, 0, 2, 3, 4];
+const arra2 = [3, 5, 6, 7, 8, 13];
+
+function dodavanje(par1, par2) {
+  const duzina = par1.length > par2.length ? par1.length : par2.length;
+  const novii = [];
+  for (i = 0; i < duzina; i++) {
+    if (par1[i] === undefined) {
+      novii.push(par2[i]);
+    } else if (par2[i] === undefined) {
+      novii.push(par1[i]);
+    } else {
+      novii.push(par1[i] + par2[i]);
+    }
+  }
+  return novii;
+}
