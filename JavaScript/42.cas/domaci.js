b1ce9ed69e759c86f347895159744f26e6f6e48b
UBaksa <ujkanovicbakir03@gmail.com>
2023-02-02 23:07:20 +0100

    domaci.js42

--- JavaScript/42.cas/domaci.js
+++ JavaScript/42.cas/domaci.js
@@ -33,3 +33,41 @@ const library = [
 //   libraryID: 1254,
 //   title: "Bill Gates"
 // }]
+// / 1. Nacin
+function sort() {
+  max = library[0].title.length;
+  for (i = 1; i < library.length; i++) {
+    if (library[i].title.length > max) {
+      max = library[i].title.length;
+    }
+  }
+  for (i = 0; i < library.length - 1; i++) {
+    for (j = i + 1; j < library.length; j++) {
+      for (k = 0; k < max; k++) {
+        if (library[i].title[k] > library[j].title[k + 1]) {
+          pom = library[i];
+          library[i] = library[j];
+          library[j] = pom;
+        }
+      }
+    }
+  }
+  console.log(library);
+}
+console.log(sort());
+
+// 2. Nacin
+
+const sort2 = (arr) => {
+  const newArr = arr.map((element) => element.title);
+  newArr.sort();
+  const finalArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.find((element) => element.title === newArr[i])) {
+      finalArr.push(arr.find((element) => element.title === newArr[i]));
+    }
+  }
+
+  return finalArr;
+};
+console.log(sort2(library));
