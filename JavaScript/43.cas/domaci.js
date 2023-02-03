dfbcff3c0a4f3116d6fd8acf19f04b42c1002107
UBaksa <ujkanovicbakir03@gmail.com>
2023-02-03 23:08:02 +0100

    domaci js

---
+++ JavaScript/43.cas/domaci.js
@@ -0,0 +1,33 @@
+// Domaci:
+const automobil = {
+  marka: "Audi",
+  model: "Q7",
+  boja: "Bela",
+  pogon: "quattro",
+  menjac: "Automatik",
+  km: 240000,
+  vlasnik: ["062321552", "063930630"],
+  garaza: {
+    parking: "JKP Servis",
+    vikend: "od 17 free",
+    satnaKarta: 50,
+    dnevnaKarta: 200,
+    mesecnaKarta: 2000,
+  },
+};
+// Napraviti funkciju koja ce povecati vrednost satne, dnevne i mesecne karte za po 20%.
+// Za bilo koji objekat koji ima properties: satnaKarta, dnevnaKarta i mesecnaKarta.
+// Nakon toga je primeniti za automobil objekat i ispisati automobil nakon toga.
+
+function povecanjes(element) {
+  return (element.garaza.satnaKarta *= 1.2);
+}
+console.log(povecanjes(automobil));
+function povecanjednevne(element) {
+  return (element.garaza.dnevnaKarta *= 1.2);
+}
+console.log(povecanjednevne(automobil));
+function povecanjemesecne(element) {
+  return (element.garaza.mesecnaKarta *= 1.2);
+}
+console.log(povecanjemesecne(automobil));
